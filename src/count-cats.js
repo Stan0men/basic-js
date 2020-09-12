bfa469616e619141f36e1b3b2ae000059b6406cd
Stan0men <stan0men@yahoo.com>
2020-09-12 09:56:40 +0300

    count-cats done

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(multiArr) {
   // remove line with error and write your code here
+let count = 0;
+const catEars = "^^";
+
+for(let i = 0; i < multiArr.length; i++){
+    for(let j = 0; j < multiArr[i].length; j++){
+        if(multiArr[i][j] === catEars){
+            count++;
+        }
+
+    }
+}
+return count;
 };
